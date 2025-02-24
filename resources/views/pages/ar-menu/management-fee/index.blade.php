c18c12044a0109d0e1b1026195e5345e240be1f5
hanhafizh <farhanhafizh770@gmail.com>
2025-02-24 16:34:36 +0700

    manfee: update show, create, edit, flow abis submit docs baru langsung mengarah ke edit

--- resources/views/pages/ar-menu/management-fee/index.blade.php
+++ resources/views/pages/ar-menu/management-fee/index.blade.php
@@ -3,22 +3,16 @@
 
         <!-- Dashboard actions -->
         <div class="sm:flex sm:justify-between sm:items-center mb-8">
-
             <!-- Left: Title -->
             <div class="mb-4 sm:mb-0">
                 <h1 class="text-2xl md:text-3xl text-gray-800 dark:text-gray-100 font-bold">AR - Management Fee</h1>
             </div>
-
             <!-- Right: Actions -->
             <div class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
-
             </div>
-
             <x-button type="button" onclick="window.location='{{ route('management-fee.create') }}'">
                 + Data Baru
             </x-button>
-
-
         </div>
 
         <!-- Cards -->
--- resources/views/pages/ar-menu/management-fee/invoice-detail/show.blade.php
+++ resources/views/pages/ar-menu/management-fee/invoice-detail/show.blade.php
@@ -12,82 +12,62 @@
         <x-management-fee.header :transaction_status="null" :document_status="null" :category="$category" isShowPage="true" />
 
 
-        {{-- Akumulasi Biaya --}}
-        {{-- Tittle Akumulasi Biaya --}}
-        <div class="mb-5 sm:flex sm:justify-between sm:items-center ">
-            <!-- Left: Title -->
-            <div class="sm:mb-0">
-                <h5 class="text-sm font-semibold text-gray-900 dark:text-gray-100">
-                    Akumulasi Biaya
-                </h5>
-            </div>
-        </div>
-        {{-- Tittle Akumulasi Biaya End --}}
-        <div class="mt-5 md:mt-0 md:col-span-2 mb-8">
+        {{-- AKUMULASI BIAYA --}}
+        <div class="mt-5 mb-5 md:mt-0 md:col-span-2">
+            <h5 class="text-sm font-semibold text-gray-900 dark:text-gray-100 mb-5">
+                Akumulasi Biaya
+            </h5>
             <div class="px-4 py-5 sm:p-6 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <div class="grid grid-cols-1 sm:grid-cols-2 gap-6">
 
-                {{-- Akumulasi Biaya Form --}}
-                <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-4">
-                    <div class="col-span-1 sm:col-span-2 lg:col-span-3">
-                        <x-label for="test" value="{{ __('Akun') }}" />
-                        <x-input id="test" type="text" class="mt-1 block w-full min-h-[40px]"
-                            wire:model.live="state.test" required autocomplete="test" />
-                        <x-input-error for="test" class="mt-2" />
+                    {{-- Akun --}}
+                    <div class="col-span-1">
+                        <x-label for="akun" value="{{ __('Akun') }}" />
+                        <p class="text-gray-800 dark:text-gray-200"></p>
                     </div>
 
-                    <div class="col-span-1 sm:col-span-2 lg:col-span-2 lg:col-start-4">
-                        <x-label for="test" value="{{ __('Rate Manfee') }}" />
-                        <x-input id="test" type="text" class="mt-1 block w-full min-h-[40px]"
-                            wire:model.live="state.test" required autocomplete="test" />
-                        <x-input-error for="test" class="mt-2" />
+                    {{-- DPP Pekerjaan --}}
+                    <div class="col-span-1">
+                        <x-label for="dpp_pekerjaan" value="{{ __('DPP Pekerjaan') }}" />
+                        <p class="text-gray-800 dark:text-gray-200"></p>
                     </div>
 
-                    <div class="col-span-1 sm:col-span-2 lg:col-span-3 lg:col-start-6">
-                        <x-label for="test" value="{{ __('Nilai Manfee') }}" />
-                        <x-input id="test" type="text" class="mt-1 block w-full min-h-[40px]"
-                            wire:model.live="state.test" required autocomplete="test" />
-                        <x-input-error for="test" class="mt-2" />
+                    {{-- RATE PPN --}}
+                    <div class="col-span-1 sm:col-span-1">
+                        <x-label for="rate_ppn" value="{{ __('RATE PPN') }}" />
+                        <p class="text-gray-800 dark:text-gray-200"></p>
                     </div>
 
-                    <div class="col-span-1 sm:col-span-3 lg:col-span-5 lg:row-start-2">
-                        <x-label for="test" value="{{ __('DPP') }}" />
-                        <x-input id="test" type="text" class="mt-1 block w-full min-h-[40px]"
-                            wire:model.live="state.test" required autocomplete="test" />
-                        <x-input-error for="test" class="mt-2" />
+                    {{-- NILAI PPN --}}
+                    <div class="col-span-1 sm:col-span-1">
+                        <x-label for="nilai_ppn" value="{{ __('NILAI PPN') }}" />
+                        <p class="text-gray-800 dark:text-gray-200"></p>
                     </div>
 
-                    <div class="col-span-1 sm:col-span-3 lg:col-span-5 lg:row-start-3">
-                        <x-label for="test" value="{{ __('RATE PPN') }}" />
-                        <x-input id="test" type="text" class="mt-1 block w-full min-h-[40px]"
-                            wire:model.live="state.test" required autocomplete="test" />
-                        <x-input-error for="test" class="mt-2" />
+                    {{-- JUMLAH --}}
+                    <div class="col-span-1 sm:col-span-2">
+                        <x-label for="jumlah" value="{{ __('JUMLAH') }}" />
+                        <p class="text-gray-800 dark:text-gray-200"></p>
                     </div>
 
-                    <div class="col-span-1 sm:col-span-2 lg:col-span-5 lg:col-start-1 lg:row-start-4">
-                        <x-label for="test" value="{{ __('NILAI PPN') }}" />
-                        <x-input id="test" type="text" class="mt-1 block w-full min-h-[40px]"
-                            wire:model.live="state.test" required autocomplete="test" />
-                        <x-input-error for="test" class="mt-2" />
-                    </div>
-
-                    <div class="col-span-1 sm:col-span-2 lg:col-span-3 lg:col-start-6 lg:row-start-3">
-                        <x-label for="test" value="{{ __('JUMLAH') }}" />
-                        <x-input id="test" type="text" class="mt-1 block w-full min-h-[40px]"
-                            wire:model.live="state.test" required autocomplete="test" />
-                        <x-input-error for="test" class="mt-2" />
-                    </div>
                 </div>
-                {{-- Akumulasi Biaya Form End --}}
             </div>
         </div>
-        {{-- Akumulasi Biaya End --}}
 
         {{-- LAMPIRAN --}}
         <div class="mt-5 mb-5 md:mt-0 md:col-span-2">
-            <h5 class="text-sm font-semibold text-gray-900 dark:text-gray-100 mb-3">
-                Lampiran
-            </h5>
-
+            <div class="sm:flex sm:justify-between sm:items-center mb-5">
+                <!-- Left: Title -->
+                <div class=" sm:mb-0">
+                    <h5 class="text-sm font-semibold text-gray-900 dark:text-gray-100">Lampiran</h1>
+                </div>
+                <!-- Right: Actions -->
+                <div class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
+                </div>
+                {{-- <x-button type="button" onclick="window.location='{{ route('management-fee.create') }}'">
+                    + Data Baru
+                </x-button> --}}
+            </div>
             <div class="bg-white dark:bg-gray-800 shadow-sm rounded-xl">
                 <div class="p-3">
                     <div class="overflow-x-auto">
@@ -135,20 +115,18 @@
 
         {{-- DESKRIPSI --}}
         <div class="mt-5 mb-5 md:mt-0 md:col-span-2">
-            <h5 class="text-sm font-semibold text-gray-900 dark:text-gray-100 mb-3">
-                Deskripsi
-            </h5>
-            <div class="px-4 py-5 sm:p-6 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <p class="text-gray-800 dark:text-gray-200"></p>
+            <div class="sm:flex sm:justify-between sm:items-center mb-5">
+                <!-- Left: Title -->
+                <div class=" sm:mb-0">
+                    <h5 class="text-sm font-semibold text-gray-900 dark:text-gray-100">Deskripsi</h1>
+                </div>
+                <!-- Right: Actions -->
+                <div class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
+                </div>
+                {{-- <x-button type="button" onclick="window.location='{{ route('management-fee.create') }}'">
+                    + Data Baru
+                </x-button> --}}
             </div>
-        </div>
-
-        {{-- FAKTUR PAJAK --}}
-        <div class="mt-5 mb-5 md:mt-0 md:col-span-2">
-            <h5 class="text-sm font-semibold text-gray-900 dark:text-gray-100 mb-3">
-                Faktur Pajak
-            </h5>
-
             <div class="bg-white dark:bg-gray-800 shadow-sm rounded-xl">
                 <div class="p-3">
                     <div class="overflow-x-auto">
@@ -169,18 +147,18 @@
                             </thead>
                             <tbody class="text-sm divide-y divide-gray-100 dark:divide-gray-700/60">
                                 @php $i = 1; @endphp
-                                {{-- @foreach ($files_faktur as $file)
+                                {{-- @foreach ($attachments as $attachment)
                                     <tr>
                                         <td class="p-2 whitespace-nowrap">
                                             <div class="text-center">{{ $i++ }}</div>
                                         </td>
                                         <td class="p-2 whitespace-nowrap">
-                                            <div class="text-left">{{ $file->name }}</div>
+                                            <div class="text-left">{{ $attachment->name }}</div>
                                         </td>
                                         <td class="p-2 whitespace-nowrap">
                                             <div class="text-center flex items-center justify-center gap-2">
                                                 <x-button-action color="purple" icon="eye"
-                                                    href="{{ route('attachments.view', $file->id) }}">
+                                                    href="{{ route('attachments.view', $attachment->id) }}">
                                                     View
                                                 </x-button-action>
                                             </div>
@@ -193,6 +171,66 @@
                 </div>
             </div>
         </div>
+
+        {{-- FAKTUR PAJAK --}}
+        <div class="mt-5 mb-5 md:mt-0 md:col-span-2">
+            <div class="sm:flex sm:justify-between sm:items-center mb-5">
+                <!-- Left: Title -->
+                <div class=" sm:mb-0">
+                    <h5 class="text-sm font-semibold text-gray-900 dark:text-gray-100">Faktur</h1>
+                </div>
+                <!-- Right: Actions -->
+                <div class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
+                </div>
+                {{-- <x-button type="button" onclick="window.location='{{ route('management-fee.create') }}'">
+                    + Data Baru
+                </x-button> --}}
+            </div>
+            <div class="bg-white dark:bg-gray-800 shadow-sm rounded-xl">
+                <div class="p-3">
+                    <div class="overflow-x-auto">
+                        <table class="table-auto w-full">
+                            <thead
+                                class="text-xs font-semibold uppercase text-gray-400 dark:text-gray-500 bg-gray-50 dark:bg-gray-700 dark:bg-opacity-50">
+                                <tr>
+                                    <th class="p-2 whitespace-nowrap">
+                                        <div class="font-semibold text-center">No</div>
+                                    </th>
+                                    <th class="p-2 whitespace-nowrap">
+                                        <div class="font-semibold text-left">Nama File</div>
+                                    </th>
+                                    <th class="p-2 whitespace-nowrap">
+                                        <div class="font-semibold text-center">Action</div>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="text-sm divide-y divide-gray-100 dark:divide-gray-700/60">
+                                @php $i = 1; @endphp
+                                {{-- @foreach ($attachments as $attachment)
+                                    <tr>
+                                        <td class="p-2 whitespace-nowrap">
+                                            <div class="text-center">{{ $i++ }}</div>
+                                        </td>
+                                        <td class="p-2 whitespace-nowrap">
+                                            <div class="text-left">{{ $attachment->name }}</div>
+                                        </td>
+                                        <td class="p-2 whitespace-nowrap">
+                                            <div class="text-center flex items-center justify-center gap-2">
+                                                <x-button-action color="purple" icon="eye"
+                                                    href="{{ route('attachments.view', $attachment->id) }}">
+                                                    View
+                                                </x-button-action>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach --}}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 
         {{-- Tombol Kembali --}}
         <div class="form-group">
