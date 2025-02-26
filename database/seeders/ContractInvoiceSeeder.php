8fb3e1d5344b23d8ab92ff5bbde3dbf5fcfebabd
hanhafizh <farhanhafizh770@gmail.com>
2025-02-26 21:13:14 +0700

    seeder: add database seeder & masterbilltype

---
+++ database/seeders/ContractInvoiceSeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Contracts;
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ContractInvoiceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Contracts::insert([
+            [
+                'contract_number' => 'ADSAKDSA23',
+                'employee_name' => 'PT. Nusanida',
+                'value' => 12500000,
+                'start_date' => Carbon::now()->subMonths(3),
+                'end_date' => Carbon::now()->addMonths(9),
+                'type' => 'management_fee',
+                'path' => 'awdkasdk.png',
+                'address' => 'Jl. Merdeka No. 10, Jakarta',
+                'work_unit' => 'keuangan',
+                'status' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'contract_number' => 'ADSAKDSA24',
+                'employee_name' => 'PT. Telkom',
+                'value' => 15000000,
+                'start_date' => Carbon::now()->subMonths(6),
+                'end_date' => Carbon::now()->addMonths(6),
+                'type' => 'non_management_fee',
+                'path' => 'awdkasdk12.png',
+                'address' => 'Jl. Sudirman No. 20, Bandung',
+                'work_unit' => 'keuangan',
+                'status' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'contract_number' => 'ADSAKDSA25',
+                'employee_name' => 'PT. Indo Jaya',
+                'value' => 20000000,
+                'start_date' => Carbon::now()->subYear(),
+                'end_date' => Carbon::now()->addMonths(3),
+                'type' => 'non_management_fee',
+                'path' => 'awdkasdk12.png',
+                'address' => 'Jl. Diponegoro No. 15, Surabaya',
+                'work_unit' => 'keuangan',
+                'status' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,8 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             PermissionSeeder::class,
             MasterDataSeeder::class,
+            ContractInvoiceSeeder::class,
+            MasterBillTypeSeeder::class,
         ]);
     }
 }
