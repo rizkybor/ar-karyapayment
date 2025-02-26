d53d033ed74b0e90d7f3776c339f8eb3f77403c9
Rizky Ajie Kurniawan <86877370+rizkybor@users.noreply.github.com>
2025-02-26 14:01:12 +0700

    checkpoint modular detail component

--- resources/views/pages/ar-menu/management-non-fee/invoice-detail/show.blade.php
+++ resources/views/pages/ar-menu/management-non-fee/invoice-detail/show.blade.php
@@ -5,6 +5,7 @@
                 <h1 class="text-2xl md:text-3xl text-gray-800 dark:text-gray-100 font-bold">Detail Invoice
                     #{{ $nonManfeeDocument['invoice_number'] }}
                 </h1>
+                <p>{{ $nonManfeeDocument }}</p>
             </div>
             {{-- Tombol Kembali --}}
             <div class="form-group">
@@ -26,38 +27,33 @@
         <x-management-non-fee.header :transaction_status="$nonManfeeDocument['is_active']" :document_status="$nonManfeeDocument['status']" :document="$nonManfeeDocument" isShowPage="true" />
 
         {{-- AKUMULASI BIAYA --}}
-        <div class="mt-5 mb-5 md:mt-0 md:col-span-2">
+        {{-- <div class="mt-5 mb-5 md:mt-0 md:col-span-2">
             <h5 class="text-sm font-semibold text-gray-900 dark:text-gray-100 mb-3">
                 Akumulasi Biaya
             </h5>
             <div class="px-4 py-5 sm:p-6 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="grid grid-cols-1 sm:grid-cols-2 gap-6">
 
-                    {{-- Akun --}}
                     <div class="col-span-1">
                         <x-label for="akun" value="{{ __('Akun') }}" />
                         <p class="text-gray-800 dark:text-gray-200">{{ $nonManfeeDocument['akun'] ?? '-' }}</p>
                     </div>
 
-                    {{-- DPP Pekerjaan --}}
                     <div class="col-span-1">
                         <x-label for="dpp_pekerjaan" value="{{ __('DPP Pekerjaan') }}" />
                         <p class="text-gray-800 dark:text-gray-200">{{ $nonManfeeDocument['dpp_pekerjaan'] ?? '-' }}</p>
                     </div>
 
-                    {{-- RATE PPN --}}
                     <div class="col-span-1 sm:col-span-1">
                         <x-label for="rate_ppn" value="{{ __('RATE PPN') }}" />
                         <p class="text-gray-800 dark:text-gray-200">{{ $nonManfeeDocument['rate_ppn'] ?? '-' }}</p>
                     </div>
 
-                    {{-- NILAI PPN --}}
                     <div class="col-span-1 sm:col-span-1">
                         <x-label for="nilai_ppn" value="{{ __('NILAI PPN') }}" />
                         <p class="text-gray-800 dark:text-gray-200">{{ $nonManfeeDocument['nilai_ppn'] ?? '-' }}</p>
                     </div>
 
-                    {{-- JUMLAH --}}
                     <div class="col-span-1 sm:col-span-2">
                         <x-label for="jumlah" value="{{ __('JUMLAH') }}" />
                         <p class="text-gray-800 dark:text-gray-200">{{ $nonManfeeDocument['jumlah'] ?? '-' }}</p>
@@ -65,7 +61,10 @@
 
                 </div>
             </div>
-        </div>
+        </div> --}}
+        <x-management-non-fee.accumulated-cost.index 
+            :nonManfeeDocument="$nonManfeeDocument" 
+            :isEdit="false" />
 
         {{-- LAMPIRAN --}}
         <div class="mt-5 mb-5 md:mt-0 md:col-span-2">
