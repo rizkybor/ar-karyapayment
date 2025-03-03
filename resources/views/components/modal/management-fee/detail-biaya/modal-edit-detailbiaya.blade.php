d14d18a76722031723e918742b5ffb4dcf186c35
hanhafizh <farhanhafizh770@gmail.com>
2025-03-03 19:14:16 +0700

    manfee: detail biaya icon

--- resources/views/components/modal/management-fee/detail-biaya/modal-edit-detailbiaya.blade.php
+++ resources/views/components/modal/management-fee/detail-biaya/modal-edit-detailbiaya.blade.php
@@ -21,7 +21,7 @@
     }
 }">
     <div class="flex justify-between items-center mb-3">
-        <x-button-action class="px-4 py-2  text-white rounded-md " color="yellow" icon="eye" @click="modalOpen = true">
+        <x-button-action class="px-4 py-2  text-white rounded-md " color="yellow" icon="pencil" @click="modalOpen = true">
             Edit
         </x-button-action>
     </div>
