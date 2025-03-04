2e492724a33dbab9e09ebb1f37c530e19c38d7c2
hanhafizh <farhanhafizh770@gmail.com>
2025-03-04 15:49:27 +0700

    manfee: %

--- resources/views/components/management-fee/accumulated-costs/index.blade.php
+++ resources/views/components/management-fee/accumulated-costs/index.blade.php
@@ -58,7 +58,7 @@
 
             {{-- Rate Manfee --}}
             <div class="col-span-1 sm:col-span-2 lg:col-span-2 lg:col-start-4">
-                <x-label for="total_expense_manfee" value="{{ __('Rate Manfee') }}" />
+                <x-label for="total_expense_manfee" value="{{ __('Rate Manfee (%)') }}" />
                 @if ($isEdit)
                     <select id="total_expense_manfee" name="total_expense_manfee"
                         class="block mt-1 w-full border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-800 text-sm text-gray-700 dark:text-gray-200 font-medium px-3 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-300 dark:focus:ring-blue-700 transition-all"
