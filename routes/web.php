b346a756b1d26fa6c9e080479622be9c2ccb2337
Rizky Ajie Kurniawan <86877370+rizkybor@users.noreply.github.com>
2025-03-13 01:38:31 +0700

    checkpoint button print

--- routes/web.php
+++ routes/web.php
@@ -233,6 +233,10 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
             Route::delete('/{taxes_id}', [NonManfeeTaxController::class, 'destroy'])->name('destroy');
         });
 
+        Route::get('/{id}/print-surat', [PDFController::class, 'generateLetter'])->name('print-surat');;
+        Route::get('/{id}/print-kwitansi', [PDFController::class, 'generateInvoice'])->name('print-kwitansi');;
+        Route::get('/{id}/print-invoice', [PDFController::class, 'generateKwitansi'])->name('print-invoice');;
+
         // Route History
         Route::prefix('histories')->name('histories.')->group(function () {
             Route::get('/', [NonManfeeHistoryController::class, 'index'])->name('index');
