50c54c7de71584718846091789f4ca90e2e6635d
hanhafizh <farhanhafizh770@gmail.com>
2025-03-24 11:44:57 +0700

    manfee: create detail biaya new

--- resources/views/components/modal/management-fee/detail-biaya/modal-create-data-detailbiaya.blade.php
+++ resources/views/components/modal/management-fee/detail-biaya/modal-create-data-detailbiaya.blade.php
@@ -2,23 +2,30 @@
 
 <!-- Modal for Adding Cost Details -->
 <div x-data="{ modalOpen: false }">
-    <x-button-action class="px-4 py-2 text-white rounded-md" color="violet" @click="modalOpen = true">Tambah
-        Detail Biaya Personil</x-button-action>
+    <x-button-action class="px-4 py-2 text-white rounded-md disabled:opacity-50 disabled:cursor-not-allowed"
+        color="violet" @click="modalOpen = true" x-bind:disabled="!selectedExpenseType">
+        <span x-text="selectedExpenseType ? `+ Detail ${selectedExpenseType}` : 'Pilih Jenis Biaya'"></span>
+    </x-button-action>
+
+
     <div class="fixed inset-0 bg-gray-900 bg-opacity-30 z-50 flex items-center justify-center" x-show="modalOpen" x-cloak>
         <div class="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg max-w-md w-full"
             @click.outside="modalOpen = false">
-            <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-100 mb-4">Tambah Detail Biaya</h3>
+            <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-100 mb-4">
+                Tambah Detail <span x-text="selectedExpenseType ? selectedExpenseType : 'Pilih Jenis Biaya'"></span>
+            </h3>
             <form action="{{ route('management-fee.detail_payments.store', ['id' => $manfeeDoc->id]) }}" method="POST">
                 @csrf
                 <div class="mb-4">
-                    <label for="expense_type" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Jenis
-                        Biaya</label>
-                    <select id="expense_type" name="expense_type"
-                        class="mt-1 block w-full rounded-md border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:text-white shadow-sm focus:ring focus:ring-blue-500">
+                    {{-- <label for="expense_type" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Jenis
+                        Biaya</label> --}}
+                    <select id="expense_type" name="expense_type" class="hidden" x-model="selectedExpenseType">
+                        <option value="">Pilih Jenis Biaya</option>
                         @foreach ($jenis_biaya as $jenis_biayas)
                             <option value="{{ $jenis_biayas }}">{{ $jenis_biayas }}</option>
                         @endforeach
                     </select>
+
                 </div>
                 <div class="mb-4">
                     <label for="account"
