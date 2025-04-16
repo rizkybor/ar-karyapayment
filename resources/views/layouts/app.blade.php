f3b55423fd1e49ac761e8fdef94359dcf9e6e04e
Rizky Ajie Kurniawan <86877370+rizkybor@users.noreply.github.com>
2025-04-17 06:50:05 +0700

    update meta images

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <meta property="og:title" content="{{ $meta_title ?? 'Prime Billing - PT. Karya Prima Usahatama' }}" />
     <meta property="og:description"
         content="{{ $meta_description ?? 'Kelola dokumen, tagihan, dan approval lebih mudah dengan Prime Billing.' }}" />
-    <meta property="og:image" content="{{ asset('images/logo-kpu-ls.png') }}" />
+    <meta property="og:image" content="{{ url('images/logo-kpu-ls.png') }}" />
     <meta property="og:url" content="{{ $meta_url ?? url()->current() }}" />
     <meta property="og:type" content="website" />
     <meta property="og:site_name" content="PrimeBilling" />
