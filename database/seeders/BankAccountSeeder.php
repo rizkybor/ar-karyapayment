c216d2651e5b09cb66eb4b0707e22f6618797c88
Rizky Ajie Kurniawan <86877370+rizkybor@users.noreply.github.com>
2025-05-30 17:19:43 +0700

    update final

--- database/seeders/BankAccountSeeder.php
+++ database/seeders/BankAccountSeeder.php
@@ -13,18 +13,23 @@ class BankAccountSeeder extends Seeder
             [
                 'bank_name' => 'Bank Mandiri',
                 'account_number' => '1150099996664',
-                'account_name' => 'PT Karya Prima Usahatama',
+                'account_name' => 'Bank Mandiri KK Jakarta Gedung PGN',
             ],
             [
                 'bank_name' => 'Bank BCA',
                 'account_number' => '4017030034',
-                'account_name' => 'PT Karya Prima Usahatama',
+                'account_name' => 'Bank BCA KCP Ketapang',
             ],
             [
                 'bank_name' => 'Bank BNI',
                 'account_number' => '325949846',
-                'account_name' => 'PT Karya Prima Usahatama',
+                'account_name' => 'Bank BNI Karya Prima Usahatama',
             ],
+            [
+                'bank_name' => 'Bank Mandiri',
+                'account_number' => '1150019000019',
+                'account_name' => 'Bank Mandiri KK Jakarta Ketapang Indah',
+            ]
         ];
 
         foreach ($banks as $bank) {
