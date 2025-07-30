1f152a36f1a38631ced50ef9bff0b9f711010b6f
hanhafizh <farhanhafizh770@gmail.com>
2025-07-31 00:13:28 +0700

    nonmanfee: nama kontrak di hilangkan diganti perihal

--- resources/views/pages/ar-menu/non-management-fee/index.blade.php
+++ resources/views/pages/ar-menu/non-management-fee/index.blade.php
@@ -212,7 +212,7 @@
                                         <div class="font-semibold text-center">Jangka Waktu</div>
                                     </th> --}}
                                     <th class="p-2 whitespace-nowrap">
-                                        <div class="font-semibold text-center">Nama Kontrak</div>
+                                        <div class="font-semibold text-center">Perihal</div>
                                     </th>
                                     <th class="p-2 whitespace-nowrap">
                                         <div class="font-semibold text-center">Expired Status</div>
@@ -591,8 +591,8 @@
                     //     }
                     // },
                     {
-                        data: 'contract.title',
-                        name: 'contract.title',
+                        data: 'letter_subject',
+                        name: 'letter_subject',
                         className: 'p-2 whitespace-nowrap text-left text-sm',
                         render: function(data) {
                             return `<div>${data ?? '-'}</div>`;
@@ -652,19 +652,19 @@
                         <nav class="flex" role="navigation" aria-label="Navigation">
                             <div class="mr-2">
                                 ${currentPage > 1 ? `
-                                                                                        <button data-page="${currentPage - 2}"
-                                                                                            class="inline-flex items-center justify-center rounded-lg leading-5 px-2.5 py-2 bg-white dark:bg-gray-800 
-                                                                                            border border-gray-200 dark:border-gray-700/60 text-gray-600 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-900 shadow-sm">
-                                                                                            <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16">
-                                                                                                <path d="M9.4 13.4l1.4-1.4-4-4 4-4-1.4-1.4L4 8z" />
-                                                                                            </svg>
-                                                                                        </button>` : `
-                                                                                        <span class="inline-flex items-center justify-center rounded-lg leading-5 px-2.5 py-2 bg-white dark:bg-gray-800 
-                                                                                            border border-gray-200 dark:border-gray-700/60 text-gray-300 dark:text-gray-600 shadow-sm">
-                                                                                            <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16">
-                                                                                                <path d="M9.4 13.4l1.4-1.4-4-4 4-4-1.4-1.4L4 8z" />
-                                                                                            </svg>
-                                                                                        </span>`}
+                                                                                                <button data-page="${currentPage - 2}"
+                                                                                                    class="inline-flex items-center justify-center rounded-lg leading-5 px-2.5 py-2 bg-white dark:bg-gray-800 
+                                                                                                    border border-gray-200 dark:border-gray-700/60 text-gray-600 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-900 shadow-sm">
+                                                                                                    <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16">
+                                                                                                        <path d="M9.4 13.4l1.4-1.4-4-4 4-4-1.4-1.4L4 8z" />
+                                                                                                    </svg>
+                                                                                                </button>` : `
+                                                                                                <span class="inline-flex items-center justify-center rounded-lg leading-5 px-2.5 py-2 bg-white dark:bg-gray-800 
+                                                                                                    border border-gray-200 dark:border-gray-700/60 text-gray-300 dark:text-gray-600 shadow-sm">
+                                                                                                    <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16">
+                                                                                                        <path d="M9.4 13.4l1.4-1.4-4-4 4-4-1.4-1.4L4 8z" />
+                                                                                                    </svg>
+                                                                                                </span>`}
                             </div>
                             <ul class="inline-flex text-sm font-medium -space-x-px rounded-lg shadow-sm">`;
 
@@ -690,19 +690,19 @@
                             </ul>
                             <div class="ml-2">
                                 ${currentPage < totalPages ? `
-                                                                                        <button data-page="${currentPage}"
-                                                                                            class="inline-flex items-center justify-center rounded-lg leading-5 px-2.5 py-2 bg-white dark:bg-gray-800 
-                                                                                            border border-gray-200 dark:border-gray-700/60 text-gray-600 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-900 shadow-sm">
-                                                                                            <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16">
-                                                                                                <path d="M6.6 13.4L5.2 12l4-4-4-4 1.4-1.4L12 8z" />
-                                                                                            </svg>
-                                                                                        </button>` : `
-                                                                                        <span class="inline-flex items-center justify-center rounded-lg leading-5 px-2.5 py-2 bg-white dark:bg-gray-800 
-                                                                                            border border-gray-200 dark:border-gray-700/60 text-gray-300 dark:text-gray-600 shadow-sm">
-                                                                                            <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16">
-                                                                                                <path d="M6.6 13.4L5.2 12l4-4-4-4 1.4-1.4L12 8z" />
-                                                                                            </svg>
-                                                                                        </span>`}
+                                                                                                <button data-page="${currentPage}"
+                                                                                                    class="inline-flex items-center justify-center rounded-lg leading-5 px-2.5 py-2 bg-white dark:bg-gray-800 
+                                                                                                    border border-gray-200 dark:border-gray-700/60 text-gray-600 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-900 shadow-sm">
+                                                                                                    <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16">
+                                                                                                        <path d="M6.6 13.4L5.2 12l4-4-4-4 1.4-1.4L12 8z" />
+                                                                                                    </svg>
+                                                                                                </button>` : `
+                                                                                                <span class="inline-flex items-center justify-center rounded-lg leading-5 px-2.5 py-2 bg-white dark:bg-gray-800 
+                                                                                                    border border-gray-200 dark:border-gray-700/60 text-gray-300 dark:text-gray-600 shadow-sm">
+                                                                                                    <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16">
+                                                                                                        <path d="M6.6 13.4L5.2 12l4-4-4-4 1.4-1.4L12 8z" />
+                                                                                                    </svg>
+                                                                                                </span>`}
                             </div>
                         </nav>
                     </div>`;
